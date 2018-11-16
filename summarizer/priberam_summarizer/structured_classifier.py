b9ffe88de68feac33bfa3f6760f8b6e35f995675
Sebastião Miranda <sebastiao.miranda@priberam.pt>
2018-11-16 10:45:47 +0000

    Update 'summarizer/priberam_summarizer/structured_classifier.py'

--- summarizer/priberam_summarizer/structured_classifier.py
+++ summarizer/priberam_summarizer/structured_classifier.py
@@ -1,3 +1,9 @@
+# This module is part of “Priberam’s Summarizer”, an open-source version of SUMMA’s Summarizer module.
+# Copyright 2018 by PRIBERAM INFORMÁTICA, S.A. - www.priberam.com
+# You have access to this product in the scope of Project "SUMMA - Project Scalable Understanding of Multilingual Media", Project Number 688139, H2020-ICT-2015.
+# Usage subject to The terms & Conditions of the "Priberam  Summarizer OS Software License" available at https://www.priberam.pt/docs/Priberam_Summarizer_OS_Software_License.pdf
+
+
 """ Structured Classifier """
 import numpy as np
 from .linear_model import LinearModel
