879b554d95091247cf0105f5df35e34bd300a2b2
Sebastião Miranda <sebastiao.miranda@priberam.pt>
2018-11-16 10:44:38 +0000

    Update 'summarizer/priberam_summarizer/extractive_summarizer.py'

--- summarizer/priberam_summarizer/extractive_summarizer.py
+++ summarizer/priberam_summarizer/extractive_summarizer.py
@@ -1,3 +1,8 @@
+# This module is part of “Priberam’s Summarizer”, an open-source version of SUMMA’s Summarizer module.
+# Copyright 2018 by PRIBERAM INFORMÁTICA, S.A. - www.priberam.com
+# You have access to this product in the scope of Project "SUMMA - Project Scalable Understanding of Multilingual Media", Project Number 688139, H2020-ICT-2015.
+# Usage subject to The terms & Conditions of the "Priberam  Summarizer OS Software License" available at https://www.priberam.pt/docs/Priberam_Summarizer_OS_Software_License.pdf
+
 import math
 import multiprocessing
 from concurrent.futures import ProcessPoolExecutor
